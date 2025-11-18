64e91072a2e2b6f258ec5f7cd3f0db7588eff0a1
longngd <nguyendinhlong2112@gmail.com>
2025-11-18 16:28:16 +0700

    longnd-18112025-initProject

---
+++ src/main/java/vn/atdigital/iot/OpenmucServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.atdigital.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenmucServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenmucServiceApplication.class, args);
+    }
+
+}
